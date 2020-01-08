ea8c6c05677a2fd1b9f0dc7e1b9bee4641394411
danielpilon <danielpilon@gmail.com>
2020-01-07 22:36:52 -0300

    protein-translation solution.

---
+++ protein-translation/lib/protein_translation.ex
@@ -0,0 +1,71 @@
+defmodule ProteinTranslation do
+  @proteins %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
+
+  def of_rna(<<>>, result), do: {:ok, Enum.reverse(result)}
+
+  def of_rna(<<codon::bytes-size(3), rest::binary>>, result) do
+    case of_codon(codon) do
+      {:error, _} -> {:error, "invalid RNA"}
+      {:ok, "STOP"} -> {:ok, Enum.reverse(result)}
+      {:ok, protein} -> of_rna(rest, [protein | result])
+    end
+  end
+
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: {atom, list(String.t())}
+  def of_rna(rna) do
+    of_rna(rna, [])
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: {atom, String.t()}
+  def of_codon(codon) do
+    case @proteins do
+      %{^codon => protein} ->
+        {:ok, protein}
+
+      _ ->
+        {:error, "invalid codon"}
+    end
+  end
+end
