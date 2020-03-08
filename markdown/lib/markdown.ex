779ae49b82a307420651ea5e5102a873c706e0f1
danielpilon <danielpilon@gmail.com>
2020-03-08 18:22:12 -0300

    markdown adjustments.

--- markdown/lib/markdown.ex
+++ markdown/lib/markdown.ex
@@ -15,20 +15,20 @@ defmodule Markdown do
     do:
       m
       |> String.split("\n")
-      |> Enum.map_join(&process(&1))
+      |> Enum.map_join(&process/1)
       |> enclose_li
       |> enclose_strong
       |> enclose_em
 
-  defp process("#" <> t), do: parse_header(t)
-  defp process("* " <> t), do: "<li>" <> t <> "</li>"
-  defp process(t), do: "<p>" <> t <> "</p>"
+  defp process("#" <> text), do: parse_header(text)
+  defp process("* " <> text), do: "<li>#{text}</li>"
+  defp process(text), do: "<p>#{text}</p>"
 
-  defp parse_header(t, h \\ 1)
-  defp parse_header(" " <> t, h), do: "<h#{h}>#{t}</h#{h}>"
-  defp parse_header("#" <> t, h), do: parse_header(t, h + 1)
+  defp parse_header(header, level \\ 1)
+  defp parse_header(" " <> rest, level), do: "<h#{level}>#{rest}</h#{level}>"
+  defp parse_header("#" <> rest, level), do: parse_header(rest, level + 1)
 
-  defp enclose_strong(t), do: t |> String.replace(~r/__(.*?)__/, "<strong>\\1</strong>")
-  defp enclose_em(t), do: t |> String.replace(~r/_(.*?)_/, "<em>\\1</em>")
-  defp enclose_li(t), do: t |> String.replace(~r/(<li>.*<\/li>)/, "<ul>\\1</ul>")
+  defp enclose_strong(text), do: text |> String.replace(~r/__(.*?)__/, "<strong>\\1</strong>")
+  defp enclose_em(text), do: text |> String.replace(~r/_(.*?)_/, "<em>\\1</em>")
+  defp enclose_li(text), do: text |> String.replace(~r/(<li>.*<\/li>)/, "<ul>\\1</ul>")
 end
