7450aad3b08a4518a3527e22f234a90264e301cf
danielpilon <danielpilon@gmail.com>
2020-03-24 20:21:56 -0300

    rna-transcription solution refactor.

--- rna-transcription/lib/rna_transcription.ex
+++ rna-transcription/lib/rna_transcription.ex
@@ -8,14 +8,14 @@ defmodule RnaTranscription do
   'UGAC'
   """
   @spec to_rna([char]) :: [char]
-  def to_rna('G'), do: 'C'
-  def to_rna('C'), do: 'G'
-  def to_rna('T'), do: 'A'
-  def to_rna('A'), do: 'U'
+  def to_rna(?G), do: ?C
+  def to_rna(?C), do: ?G
+  def to_rna(?T), do: ?A
+  def to_rna(?A), do: ?U
 
   def to_rna(dna) do
     dna
-    |> Enum.map(fn l -> RnaTranscription.to_rna([l]) end)
+    |> Enum.map(&to_rna(&1))
     |> List.to_charlist()
   end
 end
