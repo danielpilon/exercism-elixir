964d277b09dbd201f5ed87a478175a1f00c1aae4
danielpilon <danielpilon@gmail.com>
2020-05-02 19:06:30 -0300

    ocr-number solution

---
+++ ocr-numbers/lib/ocr_numbers.ex
@@ -0,0 +1,103 @@
+defmodule OcrNumbers do
+  @numbers %{
+    [
+      " _ ",
+      "| |",
+      "|_|",
+      "   "
+    ] => "0",
+    [
+      "   ",
+      "  |",
+      "  |",
+      "   "
+    ] => "1",
+    [
+      " _ ",
+      " _|",
+      "|_ ",
+      "   "
+    ] => "2",
+    [
+      " _ ",
+      " _|",
+      " _|",
+      "   "
+    ] => "3",
+    [
+      "   ",
+      "|_|",
+      "  |",
+      "   "
+    ] => "4",
+    [
+      " _ ",
+      "|_ ",
+      " _|",
+      "   "
+    ] => "5",
+    [
+      " _ ",
+      "|_ ",
+      "|_|",
+      "   "
+    ] => "6",
+    [
+      " _ ",
+      "  |",
+      "  |",
+      "   "
+    ] => "7",
+    [
+      " _ ",
+      "|_|",
+      "|_|",
+      "   "
+    ] => "8",
+    [
+      " _ ",
+      "|_|",
+      " _|",
+      "   "
+    ] => "9"
+  }
+
+  @doc """
+  Given a 3 x 4 grid of pipes, underscores, and spaces, determine which number is represented, or
+  whether it is garbled.
+  """
+  @spec convert([String.t()]) :: {:ok, String.t()} | {:error, charlist()}
+  def convert(input) when rem(length(input), 4) > 0, do: {:error, 'invalid line count'}
+
+  def convert([first | _rest]) when rem(byte_size(first), 3) > 0,
+    do: {:error, 'invalid column count'}
+
+  def convert(input) do
+    input
+    |> Enum.chunk_every(4)
+    |> Enum.map_join(",", &convert_to_numbers/1)
+    |> (fn numbers -> {:ok, numbers} end).()
+  end
+
+  defp convert_to_numbers(input) do
+    input
+    |> Enum.map(&split_every_3/1)
+    |> Enum.zip()
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.map(&do_convert/1)
+    |> to_number("")
+  end
+
+  defp split_every_3(string), do: Regex.scan(~r/.../, string) |> List.flatten()
+
+  defp to_number([], acc), do: acc
+  defp to_number([number | rest], acc), do: to_number(rest, acc <> number)
+
+  defp do_convert(input) do
+    with {:ok, number} <- Map.fetch(@numbers, input) do
+      number
+    else
+      _err -> "?"
+    end
+  end
+end
